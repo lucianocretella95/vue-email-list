ff1653bab07adee7296dcef7703a59b3e7d4e72f
lucianocretella95 <luciano.cretella@tim.it>
2024-05-15 17:43:11 +0200

    comunicazione con API in console

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ createApp({
   data() {
     return {
       message: "Generare 10 mail con API",
-      mail_api: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      mail:"https://flynn.boolean.careers/exercises/api/random/mail",
     };
   },
 
   methods: {
     getmail() {
-      axios.get(this.mail_api).then((result) => {
-        console.log(this.mail_api);
+      axios.get(this.mail).then((result) => {
+        console.log(result);
       });
     },
   },
