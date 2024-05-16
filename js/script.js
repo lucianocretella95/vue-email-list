c03f038174de543ca9e54771e2e60ec4b7e8b631
lucianocretella95 <luciano.cretella@tim.it>
2024-05-16 10:39:00 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -3,16 +3,26 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      // usare THIS !!!!!!!
       message: "Generare 10 mail con API",
-      mail:"https://flynn.boolean.careers/exercises/api/random/mail",
+      mail: "https://flynn.boolean.careers/exercises/api/random/mail",
+      newmail: [],
     };
   },
 
   methods: {
     getmail() {
-      axios.get(this.mail).then((result) => {
-        console.log(result);
-      });
+      this.newmail = []
+      for (let i = 0; i < 10; i++) {
+        axios.get(this.mail).then((result) => {
+          console.log(result.data.response);
+          this.newmail.push(result.data.response);
+        });
+      }
     },
+  },
+  mounted() {
+    console.log("app caricata");
+    // this.getmail();
   },
 }).mount("#app");
